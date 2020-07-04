5b3d9daf3bafdab492b5ddee72877df53b3380fe
securethebags <anthonyjim96@gmail.com>
2020-07-04 13:32:55 -0400

    created utils, added examples of mutation to arrays

--- simpleIterator.js
+++ simpleIterator.js
@@ -1,30 +1,6 @@
-// from(), to(), and fromTo() are used in conjuction to create a system in which we can
-// iterate through data structures
-
-function from(arg) {
-  return () => {
-    arg += 1;
-    return arg - 1;
-  };
-}
-
-const from2 = from(2);
-console.log(from2()); // unbounded iterator
-
-function to(generator, limiter) {
-  return () => {
-    if (limiter === 0) return null;
-    limiter -= 1;
-    return generator();
-  };
-}
-
-function fromTo(x, y) {
-  return to(from(x), y);
-}
+const { fromTo } = require("./utils");
 
 const fromOneToThree = fromTo(1, 3); // new functions can be created with seperate Closed Over Variables bc of closure
-const from0To4 = fromTo(0, 4);
 
 function element(array, func) {
   if (func === undefined) {
@@ -33,7 +9,7 @@ function element(array, func) {
   }
   return () => {
     const currentIndex = func();
-    return !currentIndex ? undefined : array[func()]; // if currentIndex DNE return undefined otherwise return array element
+    return !currentIndex ? undefined : array[currentIndex]; // if currentIndex DNE return undefined otherwise return array element
   };
 }
 
@@ -45,3 +21,5 @@ console.log(ele()); // "b"
 console.log(ele()); // "c"
 console.log(ele()); //  "d"
 console.log(ele()); // undefined
+
+// more to come!
